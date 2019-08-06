1274a47bb6a92af2e585d9e5455bf7e46a684afd
Diego <dev@datweb.com.br>
2019-08-06 08:11:42 -0400

    version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { VendaComponent } from './venda/venda.component';
+import { ClientesComponent } from './clientes/clientes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+{ path: 'dashboard', component: DashboardComponent },
+{ path: 'formulario', component: FormularioComponent },
+{ path: 'venda', component: VendaComponent },
+{ path: 'clientes', component: ClientesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { HeaderComponent } from './header/header.component';
+import { VendaComponent } from './venda/venda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    FormularioComponent,
+    ClientesComponent,
+    HeaderComponent,
+    VendaComponent
   ],
   imports: [
     BrowserModule,
